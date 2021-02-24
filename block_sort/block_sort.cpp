1f6d42769d67502f34dbac65c14036062632734e
unknown <romanmax82@gmail.com>
2021-02-24 15:07:38 +0300

    Update

--- block_sort/block_sort.cpp
+++ block_sort/block_sort.cpp
@@ -3,28 +3,48 @@
 #include "Sort.h"
 using namespace std;
 
+void print_arr(vector<int>& v, int x)
+{
+    for (int i = 0; i < v.size(); i += x)
+        cout << v[i] << " ";
+}
+
 int main()
 {
-    vector<int> v;
-    srand(time(0));
-    for (int i = 0; i < 5000; ++i)
-        v.push_back(rand() % 100000 - 3000);
+    vector<int> v1;
+    vector<int> v2;
+    int step = 500;
 
-    for (auto x : v)
+    srand(time(0));
+    for (int i = 0; i < 10000; ++i)
     {
-        cout << x << " ";
-    }
-    cout << "\narray size: " << v.size() << endl;
+        v1.push_back(rand() % 1000 - 500);
+        v2.push_back(rand() % 1000 - 500);
+    }   
+
+    cout << "v1: ";
+    print_arr(v1, step);
+    cout << endl;
+    cout << "v2: ";
+    print_arr(v2, step);
+    cout << endl;
+    
     cout << "===========================================================" << endl;
 
     Sort a;
-    a.block_sort(v);
+    // block sorting and calculation of runtime 
+    a.block_sort(v1);
+    // bubble sorting and calculation of runtime
+    a.bubble_sort(v2);
+    cout << "===========================================================" << endl;
+    cout << endl;
 
-    for (auto x : v)
-    {
-        cout << x << " ";
-    }
-    cout << "\narray size: " << v.size() << endl;
+    cout << "v1: ";
+    print_arr(v1, step);
+    cout << endl;
+    cout << "v2: ";
+    print_arr(v2, step);
+    cout << endl;
 
 
     return 0;
